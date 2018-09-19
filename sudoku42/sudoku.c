78de6b7cb40119696c58ccbb02ebb03451c80c70
midish <zshykov@gmail.com>
2018-09-19 23:12:52 +0300

    Hi

--- sudoku42/sudoku.c
+++ sudoku42/sudoku.c
@@ -7,44 +7,86 @@ int     **preobrazovanieStrok(char **str)
     int x = 0;
     int y = 0;
     int **digitMatrix = NULL;
-    int digit = 0;
+    // int digit = 0;
 
     // Выделяю память под массив из ЦИФР
-    digitMatrix = (int **) malloc(sizeof(int *) * 9);
+    digitMatrix = (int **) malloc(9 * sizeof(int*));
     while (i < 9)
     {
-        digitMatrix[i] = (int *) malloc(sizeof(int *) * 9);
+        digitMatrix[i] = (int *) malloc(9 * sizeof(int));
         i++;
     }
     while (x < 9)
-    {
-        digitMatrix[x] = str[x];
-        x++;
+    {   
         while (y < 9)
         {
-            if (str[x][y] <= '0' || '9' >= str[x][y])
+            if (str[x][y] >= '1' && '9' >= str[x][y])
             {
-                digit = str[x][y] - '0';
-                digitMatrix[x][y] = digit;
-            }
+                digitMatrix[x][y] = str[x][y] - '0';
+            }  
             else if (str[x][y] == '.')
             {
-                digit = 0;
-                digitMatrix[x][y] = digit;
+                digitMatrix[x][y] = 0;
             }
             y++;
         }
+        if (y == 9)
+        {
+            y = 0;
+        }
+        x++;
     }
-    
+    return (digitMatrix);
 }
+
+// int     main(void)
+// {
+//     char *str1[9] = { "111111111", "222222222", "333333333", "444444444", "555555555", "666666666", "777777777", "888888888", "999999999" };
+//     int **strNbr = NULL;
+//     int i = 0;
+//     int j = 0;
+
+//     strNbr = preobrazovanieStrok(str1);
+//     while (i < 9)
+//     {
+//         while (j < 9)
+//         {
+//             printf("%d", strNbr[i][j]);
+//             j++;
+//         }
+//         i++;
+//         if (j == 9)
+//         {
+//             printf("\n");
+//             j = 0;
+//         }
+//     }
+//     return (0);
+// }
 
 int     main(int argc, char **argv)
 {
     int **sudokuMatrix = NULL;
-
+    int i = 0;
+    int j = 0;
+    
     if (argc == 10)
     {
-        sudokuMatrix = preobrazovanieStrok(argv);
+        sudokuMatrix = preobrazovanieStrok(&argv[1]);
+        while (i < 9)
+        {
+            while (j < 9)
+            {
+                printf("%d", sudokuMatrix[i][j]);
+                j++;
+            }
+            i++;
+            if (j == 9)
+            {
+                printf("\n");
+                j = 0;
+            }
+        }
     }
     else
     {
