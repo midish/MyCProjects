b94adc18bfa6ca456c4f90d1e3d72667862a4ef6
midish <zshykov@gmail.com>
2018-11-06 21:11:12 +0300

    Так

--- sastantua/pyramidka.c
+++ sastantua/pyramidka.c
@@ -11,14 +11,16 @@ void    ft_putchar(char c)
 void    pyramidka(int size)
 {
     int i;
-    int weight;
+    int weight; // Ширина строки в символах
     int j;
 
     i = 1;
     weight = 1;
+    // Общий цикл который печатает уровни пирамиды, начиная с первого
     while(i <= size)
     {
         j = 0;
+        // Цикл выводит кол-во символов на строке
         while(j < weight)
         {
             ft_putchar('A');
